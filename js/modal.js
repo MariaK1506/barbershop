c58cacf7e193937c8884d12fb9abf026c02ee2fd
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-09-25 22:25:03 +0300

    correct styles

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,20 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtn2: document.querySelector("[data-modal-open2]"),
+    openModalBtn4: document.querySelector("[data-modal-open4]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn2.addEventListener("click", toggleModal);
+  refs.openModalBtn4.addEventListener("click", toggleModal);
+
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
